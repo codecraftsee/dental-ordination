83fc0fd8a414e408b757b2f2232e99cfd2032705
codecraftsee <miodrag@codecraftsolutions.rs>
2026-02-20 14:56:57 +0100

    build error fix

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,7 +8,7 @@ import { ThemeService } from './services/theme.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TranslatePipe, LanguageSwitcher, Sidebar],https://github.com/codecraftsee/dental-ordination/pull/6/conflict?name=src%252Fapp%252Fhome%252Fhome.ts&ancestor_oid=2b2c7cef4ea7951c6bcfc8f538edabf62a94dc0d&base_oid=d40bfa4daf7ea5fd8c3d5f3d96e20473787cca95&head_oid=cec20ca307daa1dff7fcd4105e3007afbd765b39
+  imports: [RouterOutlet, TranslatePipe, LanguageSwitcher, Sidebar],
   templateUrl: './app.html',
   styleUrl: './app.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
