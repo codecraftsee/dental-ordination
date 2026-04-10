bfd515f9e1478af1d96fdbcf7c367e4c4c8f96f4
codecraftsee <miodrag@codecraftsolutions.rs>
2026-04-10 16:18:49 +0200

    api utl update to point to railway

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://dental-ordination-api.onrender.com',
+  apiUrl: 'https://dental-ordination-api-production.up.railway.app/api',
 };
